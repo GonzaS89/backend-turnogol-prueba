fdd8e9b18392e020b9920255ff83b9eb932b2ea4
Gonza <gnzx89@gmail.com>
2025-06-06 12:40:10 -0300

    first commit

--- index.js
+++ index.js
@@ -174,6 +174,7 @@ app.delete("/api/pruebas/turnos_canchas/:id", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Backend corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend corriendo en http://0.0.0.0:${PORT}`);
 });
+
