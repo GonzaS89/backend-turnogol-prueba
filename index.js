dc03f16e877c5f1646fef94d388a1f079c5fd75c
Gonza <gnzx89@gmail.com>
2025-06-06 12:08:27 -0300

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 dotenv.config()
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors({ origin: '*' }));
 
